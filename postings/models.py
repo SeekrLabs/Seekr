afd1a4192d83e3f27d5de53d1af197b8a9ec6136
jhunnie <jenniej.hu@mail.utoronto.ca>
2020-01-20 11:49:57 -0500

    unclear whether or not drawing this picture will work the way i want it to but i'll commit for now

--- postings/models.py
+++ postings/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import date
+from PIL import Image, ImageDraw
 
 class Posting(models.Model):
     id = models.CharField(max_length=64, primary_key=True, editable=False)
@@ -12,6 +13,11 @@ class Posting(models.Model):
     vector = models.CharField(max_length=2048, default='')
     description = models.CharField(max_length=256, default='')
     image_url = models.URLField(blank=True)
+    # logo = models.URLField(default='')
+    
+    # default_logo = default.jpg
+    # will need to add this to produce images
+    # need to have default "no image" jpeg
 
     def get_image_url(self):
         if self.image_url is None:
@@ -21,7 +27,23 @@ class Posting(models.Model):
 
     def generate_image(self):
         # title = self.tile to access parameters
-        pass
+        # each posting has company logo, job title, experience level, location, and posting link
+        #img = Image.new('RGB', (570, 430), color = (255, 255, 255))
+        if self.logo is None: 
+            logo = default_logo
+            self.logo = self.save_logo(logo)
+        img = Image.open(self.logo_url) 
+        #i'm not entirely sure this will work lmao 
+
+        #draw the image 
+        draw = ImageDraw.Draw(img)
+        draw.line((0, 0) + img.size, fill=(0,0,0))
+        draw.text((0, 0) + img.size, self.title, fill=(0,0,0))
+        draw.text((0, 0) + img.size + 20, self.company + "• Full Time", fill = (0,0,0))
+        draw.line((0, 0) + img.size + 60, fill = (0,0,0))
+        url = id + company + title + ".jpg"
+        img.save(url)
+        #pass
 
     def save_image(self, image):
         """ 
@@ -34,6 +56,9 @@ class Posting(models.Model):
             image_url (string): a publically access url string
         """
         pass
+    
+    def save_logo(self, logo):
+        #save logo to amazon s3 + db 
 
     def __str__(self):
         return "%s %s %s   " % (self.company, self.title, self.date_posted)
