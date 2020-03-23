929ca04f605db7fdfdd14f35486b205b4f2283ac
russellJacob <23370169+russellJacob@users.noreply.github.com>
2020-03-23 09:23:47 -0400

    Sk 10 (#18)
    * Added function to turn website URL into logo
    
    * Updated load_logo function
    
    * Download logo from company name
    
    * Update models.py
    
    * Update models.py
    
    * Changed load_logo to return s3 URL (assuming us east bucket)
    
    * Did some cleaning up of load_logo
    
    * Added S3 load environ variables
    
    * Removed Key environ variables
    
    * Update models.py
    
    * Moved s3 client init to global
    
    * Initialize boto3 client
    
    * Update models.py
    
    * Added boto3
    
    * No longer download the logo
    
    * Imported botocore
    
    * Update requirements.txt
    
    * Check if logo exists in s3 before using API
    
    * Update models.py
    
    * Fixed Small URL problem
    
    * Small variable name changes
    
    Co-authored-by: Walton Wang <waltonwang98@gmail.com>

--- global_variables.py
+++ global_variables.py
@@ -1,9 +1,13 @@
 from profiles.scraper import LinkedInScraper
 from profiles.linkedin import LinkedIn
 import sys
+import boto3
+from constants import AWS_ACCESS_KEY, AWS_SECRET_KEY
 
 linkedin_scraper = None
 linkedin = None
+
+s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
 
 if any(command in sys.argv for command in ['runserver', 'Seekr.wsgi:application', 'ingest']):
     linkedin_scraper = LinkedInScraper(headless=True)
--- postings/models.py
+++ postings/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from datetime import date
 from PIL import Image, ImageDraw
+import urllib.request
+import json
+import boto3
+import botocore
+import os
+from constants import *
+from global_variables import s3
+import requests
+from io import BytesIO
+
 import numpy as np
 from profiles.models import Profile
 import logging
 
 logger = logging.getLogger('app')
+
 
 class Posting(models.Model):
     id = models.CharField(max_length=64, primary_key=True, editable=False)
@@ -95,6 +106,60 @@ class Posting(models.Model):
         """
         pass
 
+    #Takes in a company name and bucket location for upload
+    #and uploads the file to S3, and returns the s3 url
+    def load_logo(self, company):
+        """Loads a company logo based on company name
+
+        Queries bitbucket api to find a logo from partial company names, and saves the logo url to s3. 
+
+        Args:
+            company (string): The company name to find the logo for
+
+        Returns:
+            Returns the URL that a user can query to get the logo from S3. 
+        """
+        
+        
+        #Declare Bucket to save to
+        bucket = "seekr-company-logo-image"
+        
+        #Check if url is already in s3
+        image_filename = company + ".png"
+        s3_url_attempt = "https://" + bucket + ".s3.us-east-2.amazonaws.com/" + image_filename
+        #Use url to check if already in s3
+        is_present = True
+        
+        s3_resource = boto3.resource('s3')
+        try:
+            s3_resource.Object(bucket, image_filename).load()
+        except botocore.exceptions.ClientError as error:
+            is_present = False
+        
+        if is_present:
+            return s3_url_attempt
+        
+        #Get company autocomplete and values
+        url = "https://autocomplete.clearbit.com/v1/companies/suggest?query=" + company
+        search_result = urllib.request.urlopen(url)
+        data = search_result.read()
+        encoded_data = search_result.info().get_content_charset('utf-8')
+        resulting_data = json.loads(data.decode(encoded_data))
+        
+        #Create byte buffer representing logo image
+        #In the new form of upload, you need to do Image.open(byteBuffer) to access the image itself
+        item = resulting_data[0]
+        clearbit_url = item["logo"]
+        logo_response = requests.get(clearbit_url)
+        byte_buffer = BytesIO(logo_response.content)
+        
+        #Upload to S3
+        s3.upload_fileobj(byte_buffer, bucket, image_filename)
+        
+        s3Url = "https://" + bucket + ".s3.us-east-2.amazonaws.com/" + image_filename
+        
+        return s3Url
+    
     def __str__(self):
         return '{:39}{:24}{:24}{}\t{:70}'.format(self.title[:35], self.company[:20],
                 self.city[:20], self.date_posted, self.url[:70])
