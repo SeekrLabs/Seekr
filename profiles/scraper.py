d46010f0a89b96e8acc6c966d5d554787cba93ae
Joy Liu <joyliu290@hotmail.com>
2020-03-25 13:31:14 -0400

    Merged with master and ensured linkedin scraper works for single profile link

--- profiles/scraper.py
+++ profiles/scraper.py
@@ -217,6 +217,7 @@ class LinkedInScraper(SeleniumScraper):
         'DATE_RANGE': './/h4[@class="pv-entity__date-range t-14 t-black--light t-normal"]',
         'JOB_DESCRIPTION': './/p[@class="pv-entity__description t-14 t-black t-normal inline-show-more-text inline-show-more-text--is-collapsed ember-view"]',
         'EDUCATION_SINGLE': '//li[@class="pv-profile-section__list-item pv-education-entity pv-profile-section__card-item ember-view"]',
+        'EDUCATION_MULTIPLE': '//*[@class="pv-profile-section__sortable-card-item pv-education-entity pv-profile-section__card-item ember-view"]',
         'SCHOOL': './/h3[@class="pv-entity__school-name t-16 t-black t-bold"]',
         'DEGREE': './/p[@class="pv-entity__secondary-title pv-entity__degree-name t-14 t-black t-normal"]',
         'FIELD_OF_STUDY': './/p[@class="pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"]',
@@ -385,9 +386,6 @@ class LinkedInScraper(SeleniumScraper):
             return False
         username = Profile.url_to_username(profile_url)
 
-        self.scroll_to_bottom(1500) 
-        username = Profile.url_to_username(profile_url)
-
         p = Profile.objects.filter(pk=username).first()
         if p:
             logger.info("A profile with username {} exists in the database.".format(
@@ -395,6 +393,7 @@ class LinkedInScraper(SeleniumScraper):
             return p
 
         self.visit(profile_url)
+
         try:
             self.scroll_to_bottom(1500)
             name = self.get_by_xpath_text_or_none('PROFILE_NAME', wait=True)
@@ -412,9 +411,10 @@ class LinkedInScraper(SeleniumScraper):
             profile = Profile.objects.create(name=name, location=location, username=username)
             logger.info("Successfully created {}'s profile".format(username))
             
-            is_education_current = False
             for education in educations:
-                if education["end_date"] >= datetime.today():
+                is_education_current = False
+                if education["end_date"] >= datetime.now():
+                    print("whyyyy")
                     is_education_current = True
 
                 e = Education(
@@ -429,7 +429,7 @@ class LinkedInScraper(SeleniumScraper):
                     profile=profile,
                 )
                 education_list.append(e)
-            logger.info("Found education  {}".format(e))
+                logger.info("Found education  {}".format(e))
 
             Education.objects.bulk_create(education_list)
 
@@ -440,67 +440,58 @@ class LinkedInScraper(SeleniumScraper):
                 experience_date_range = experience["dates"]
                 experience_start_date = datetime.now().strftime("%Y-%m-%d")
                 experience_end_date = datetime.now().strftime("%Y-%m-%d")
+
                 experience_start_date_month = experience_date_range.split('\n')[1].split(' ')[0]
-                experience_list = []
-                for experience in experiences:
-                    is_experience_current = True
-                    # NOTE: LinkedIn requires users to input experience date range
-                    experience_date_range = experience["dates"]
-                    experience_start_date = datetime.now().strftime("%Y-%m-%d")
-                    experience_end_date = datetime.now().strftime("%Y-%m-%d")
+                    
+                if str.isdigit(experience_start_date_month): # NOTE: if user decides to not put a month for start date
+                    experience_start_date_year = experience_start_date_month
+                    experience_start_date = datetime.strptime("Jan" + " 01 " + experience_start_date_year, '%b %d %Y')
+                else:
+                    experience_start_date_year = experience_date_range.split('\n')[1].split(' ')[1]
+                    experience_start_date = datetime.strptime(experience_start_date_month + " 01 " + experience_start_date_year, '%b %d %Y')
 
-                    experience_start_date_month = experience_date_range.split('\n')[1].split(' ')[0]
-                        
-                    if str.isdigit(experience_start_date_month): # NOTE: if user decides to not put a month for start date
-                        experience_start_date_year = experience_start_date_month
-                        experience_start_date = datetime.strptime("Jan" + " 01 " + experience_start_date_year, '%b %d %Y')
-                    else:
-                        experience_start_date_year = experience_date_range.split('\n')[1].split(' ')[1]
-                        experience_start_date = datetime.strptime(experience_start_date_month + " 01 " + experience_start_date_year, '%b %d %Y')
+                try: # this part is for handling the end-date edge cases
+                    experience_end_date_month = experience_date_range.split('\n')[1].split(' ')[2]
 
-                    try: 
+                except IndexError: # NOTE: when user only put a year for both start and end date
+                    experience_end_date = datetime.strptime("Dec" + " 01 " + experience_start_date_year, '%b %d %Y')
+                else:
+                    try:
+                        experience_end_date_month = experience_date_range.split('\n')[1].split(' ')[3]
+                    except IndexError: # NOTE: this means that user either put present or just put a year for end date
                         experience_end_date_month = experience_date_range.split('\n')[1].split(' ')[2]
+                    else: 
+                        experience_end_date_month = experience_date_range.split('\n')[1].split(' ')[3]
 
-                    except IndexError: # NOTE: when user only put year for both start and end date
-                        experience_end_date = datetime.strptime("Dec" + " 01 " + experience_start_date_year, '%b %d %Y')
-                    else:
-                        try:
-                            experience_end_date_month = experience_date_range.split('\n')[1].split(' ')[3]
-                        except IndexError: # NOTE: this means that user either put present or just put a year for end date
-                            experience_end_date_month = experience_date_range.split('\n')[1].split(' ')[2]
-                        else: 
-                            experience_end_date_month = experience_date_range.split('\n')[1].split(' ')[3]
+                    if experience_end_date_month != "Present":
+                        is_experience_current = False
+                        if str.isdigit(experience_end_date_month): # NOTE: if user decides to not put a month for end date
+                            experience_end_date_year = experience_end_date_month
+                            experience_end_date = datetime.strptime("Dec" + " 01 " + experience_end_date_year, '%b %d %Y')
 
-                        if experience_end_date_month != "Present":
-                            is_experience_current = False
-                            if str.isdigit(experience_end_date_month): # NOTE: if user decides to not put a month for end date
-                                experience_end_date_year = experience_end_date_month
-                                experience_end_date = datetime.strptime("Dec" + " 01 " + experience_end_date_year, '%b %d %Y')
-
-                            else:
-                                experience_end_date_year = experience_date_range.split('\n')[1].split(' ')[4]
-                                experience_end_date = datetime.strptime(experience_end_date_month + " 01 " + experience_end_date_year, '%b %d %Y')
                         else:
-                            experience_end_date = datetime.now().strftime("%Y-%m-%d")
-                    
-                    e = Experience(
-                        company=experience["company"],
-                        description=experience["description"],
-                        title = experience["title"],
-                        start_date=experience_start_date,
-                        end_date=experience_end_date,
-                        is_current=is_experience_current,
-                        profile=profile,
-                    )
-                    experience_list.append(e)
-                    logger.info("Found experience {}".format(e))
+                            experience_end_date_year = experience_date_range.split('\n')[1].split(' ')[4]
+                            experience_end_date = datetime.strptime(experience_end_date_month + " 01 " + experience_end_date_year, '%b %d %Y')
+                
+                e = Experience(
+                    company=experience["company"],
+                    description=experience["description"],
+                    title = experience["title"],
+                    start_date=experience_start_date,
+                    end_date=experience_end_date,
+                    is_current=is_experience_current,
+                    profile=profile,
+                )
+                experience_list.append(e)
+                logger.info("Found experience {}".format(e))
             
-                Experience.objects.bulk_create(experience_list)
+            Experience.objects.bulk_create(experience_list)
                     
-                return profile
+            return True
         except:
             self.login()
             logger.exception("Problem getting profile.")
+            return False
 
     def parse_profile_experiences(self, company, role):
         """
@@ -514,6 +505,7 @@ class LinkedInScraper(SeleniumScraper):
         if not exps:
             return None
 
+        experience_with_incorrect_company = 0
         for exp in exps:
             temp = {}
             # checking if this experience is "multiple experience under 1 company"
@@ -558,31 +550,6 @@ class LinkedInScraper(SeleniumScraper):
         # Handling the case when the user doesn't have any experience in the desired company (when user did not update LinkedIn yet):
         if len(experiences) == experience_with_incorrect_company:
             return None
-            # Multiple roles in 1 company
-            mult_roles_li = self.element_get_multiple_by_xpath_or_none(exp, 'MULTI_ROLES_LI')
-            if not mult_roles_li:
-                mult_roles_li = self.element_get_multiple_by_xpath_or_none(exp, 'MULTI_ROLES_LI_FADING')
-                
-            if mult_roles_li:
-                company = self.element_get_text_by_xpath_or_empty_string(exp, 'COMPANY_MULTI_EXPERIENCES').split('\n')[1]
-                for role in mult_roles_li:
-                    temp = {
-                        'company': company,
-                        'title': self.element_get_text_by_xpath_or_empty_string(role,
-                            'TITLE_MULTI_EXPERIENCES').split('\n')[1],
-                        'dates': self.element_get_text_by_xpath_or_empty_string(role, 'DATE_RANGE'),
-                        'description': self.element_get_text_by_xpath_or_empty_string(exp, 'JOB_DESCRIPTION')
-                    }
-                    experiences.append(temp)
-            else:
-                temp = {}
-                temp['dates'] = self.element_get_text_by_xpath_or_empty_string(exp, 'DATE_RANGE')
-                temp['title'] = self.element_get_text_by_xpath_or_empty_string(exp, 'TITLE')
-                temp['company'] = self.element_get_text_by_xpath_or_empty_string(exp, 'COMPANY')
-                temp['description'] = self.element_get_text_by_xpath_or_empty_string(exp, 'JOB_DESCRIPTION')
-                
-                if temp['dates'] and temp['title'] and temp['company']:
-                    experiences.append(temp)
 
         return experiences
 
@@ -606,7 +573,13 @@ class LinkedInScraper(SeleniumScraper):
         :return: List of dictionaries containing information about each education. Return [] if above condition does not satisfy.
         """
         educations = []
-        get_educations = self.get_multiple_by_xpath_or_none("EDUCATION_SINGLE")
+        get_educations = self.get_multiple_by_xpath_or_none("EDUCATION_MULTIPLE")
+        if not get_educations: # if there isn't multiple educations, but this person has 1 education
+            single_education_web_element = self.get_by_xpath_or_none("EDUCATION_SINGLE")
+            if not single_education_web_element:
+                return None
+            get_educations = []
+            get_educations.append(single_education_web_element)
 
         iteration = 0
         for education in get_educations:
@@ -617,22 +590,20 @@ class LinkedInScraper(SeleniumScraper):
                     education, "FIELD_OF_STUDY").split('\n')[1] if self.element_get_text_by_xpath_or_none(education, "FIELD_OF_STUDY") is not None else "" 
             degree = self.element_get_text_by_xpath_or_none(
                     education, "DEGREE").split('\n')[1] if self.element_get_text_by_xpath_or_none(education, "DEGREE") is not None else ""
-            
-            # degree = degree.lower()
 
-            # if "bachelor" in degree or "bcs" in degree or "BSC" in degree :
-            #     degree = "Bachelor"
-            # elif "master" in degree:
-            #     degree = "Master"
-            # elif "phd" in degree:
-            #     degree = "PhD"
-            # else:
-            #     degree = "Othe
+            if "bachelor" in degree.lower():
+                degree = "Bachelor"
+            elif "master" in degree.lower():
+                degree = "Master"
+            elif "phd" in degree.lower():
+                degree = "PhD"
+            else:
+                degree = "Other"
 
             temp["degree"] = degree
             temp["grade"] = self.element_get_text_by_xpath_or_none(
                     education, "GRADE").split('\n')[1] if self.element_get_text_by_xpath_or_none(education, "GRADE") is not None else ""
-            
+
             start_date = ""
             end_date = ""
             school_date_range = self.element_get_text_by_xpath_or_none(
@@ -645,19 +616,18 @@ class LinkedInScraper(SeleniumScraper):
                     school_date_range = ""
                 else:
                     end_date = datetime(int(school_date_range.split('\n')[1].split(' ')[2]), 5, 1)
-                
+
             temp["start_date"] = start_date
             temp["end_date"] = end_date
+            print("end date", temp["end_date"])
 
             temp["description"] = self.element_get_text_by_xpath_or_none(
                     education, "EDUCATION_DESCRIPTION") if self.element_get_text_by_xpath_or_none(education, "EDUCATION_DESCRIPTION") is not None else ""
 
-            if iteration == 0 and (temp["institution"] == ""
-                    or temp["field_of_study"] == "" or school_date_range == ""):
-                return []
-            elif iteration>=0 and (temp["institution"] != ""
-                    and temp["field_of_study"] != "" and school_date_range != ""):
+            if iteration==0 and (temp["institution"] == "" or temp["field_of_study"] == "" or school_date_range == ""):
+                return None
+            elif iteration>=0 and (temp["institution"] != "" and temp["field_of_study"] != "" and school_date_range != ""):
                 educations.append(temp)
             iteration+=1
 
-        return educations
+        return educations 
